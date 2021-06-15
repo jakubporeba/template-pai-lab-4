c218b4be97a88c73ff0850d9d0ea3adcc2f8f4a2
Jakub Poręba <jakub.poreba@student.wat.edu.pl>
2021-06-15 14:08:21 +0200

    Zadanie 4.4 – rejestracja użytkownika

--- templates/mainTemplate.php
+++ templates/mainTemplate.php
@@ -44,6 +44,7 @@
           break;
         case 'showRegistrationForm':
           //Wyświetlenie formularza wyszukiwania
+          $portal->showRegistrationForm();
           break;
         case 'showSearchForm':
           //Wyświetlenie formularza rejestracyjnego
